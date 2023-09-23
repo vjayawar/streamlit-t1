0280b878f1a906a3c9039a27f6545d5287d3a955
vjayawar <78444453+vjayawar@users.noreply.github.com>
2023-09-23 12:25:42 +0000

    Test data generation

--- Hello.py
+++ Hello.py
@@ -1,34 +1,46 @@
+from urllib.error import URLError
+
+import numpy as np
+import pandas as pd
+import random
 
 import streamlit as st
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Data Test",
+        page_icon="->",
     )
+
+    rng = np.random.default_rng(98765)
+    random.seed(98765)
+
+    capability = ["client", "connectivity","trading","middle_office","settlements"]
+
+    c = []     
+    for i in range(50):
+        c.append(random.choice(capability))
+
+    data = {'P1': rng.integers(low=0, high=1000000, size=50),
+            'P2': rng.integers(low=0, high=100, size=50),
+            'C' : c
+    }
+
+    df = pd.DataFrame(data)
+      
+    st.write(df)
 
     st.write("# Welcome to Streamlit! Vince  👋")
 
-    st.sidebar.success("Select a demo above.")
+    #st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
         Streamlit is an open-source app framework built specifically for
         Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
     """
     )
