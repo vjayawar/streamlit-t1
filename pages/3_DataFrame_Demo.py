5cba1c76154b3caae41c3b71aafe50840250320f
vjayawar <78444453+vjayawar@users.noreply.github.com>
2023-09-23 03:20:22 +0000

    Test removing from sidebar

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -66,7 +66,7 @@ def data_frame_demo():
 
 st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
 st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
+#st.sidebar.header("DataFrame Demo")
 st.write(
     """This demo shows how to use `st.write` to visualize Pandas DataFrames.
 (Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
